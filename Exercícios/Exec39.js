3af357c0a342c0ce3c3abaeb52f69fc86ec1e528
Andrey Hiemer <100521047+Hiemer23@users.noreply.github.com>
2022-11-29 16:23:07 -0300

    Update Exec39.js

--- Exercícios/Exec39.js
+++ Exercícios/Exec39.js
@@ -2,9 +2,12 @@
 primeiro elemento do vetorA passe a ser o primeiro elemento do vetorB e vice versa e assim sucessivamente.
 Faça a troca sem utilizar uma variável auxiliar.*/
 const trocar = (vetA, vetB) => {
-    for (let i in vetA) {
-        [vetA[i],vetB[i]]=[vetB[i],vetA[i]]
+    if(vetA.length == vetB.length){
+        for (let i in vetA) {
+            [vetA[i],vetB[i]]=[vetB[i],vetA[i]]
+        }
     }
+    else return console.log("Não são de tamanhos iguais")
 }
 vetA = [1, 2, 3, 4, 5]
 vetB = [6, 7, 8, 9, 10]
