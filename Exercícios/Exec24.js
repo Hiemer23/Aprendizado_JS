681ef9210d264b6249bec277c34a551204d07cd7
Andrey Hiemer <100521047+Hiemer23@users.noreply.github.com>
2022-11-29 10:53:24 -0300

    Create Exec24.js

---
+++ Exercícios/Exec24.js
@@ -0,0 +1,10 @@
+/*24) Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
+*/
+function hellow(){
+    let contador =0
+    while(contador<11){
+        contador++
+        console.log(contador+" - Hellow World!")
+    }
+}
+hellow()
