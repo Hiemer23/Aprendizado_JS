6da6a7d9317983ec244e3fc8f977d1020b2bba8a
Hiemer23 <andreyhiemer23@gmail.com>
2022-12-16 11:28:16 -0300

    React - Cadastro

--- Testes e Estudos/React/calculadora/src/main/Calculator.jsx
+++ Testes e Estudos/React/calculadora/src/main/Calculator.jsx
@@ -35,7 +35,20 @@ export default class Calculator extends Component {
             const currentOperation = this.state.operation
 
             const values = [...this.state.values]
-            values[0] = eval()
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}} `)
+            } catch (e) {
+                values[0] = this.state.values[0]
+            }
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
         }
     }
 
